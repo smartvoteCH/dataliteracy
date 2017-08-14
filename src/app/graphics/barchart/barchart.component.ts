5f28dc0db746c30053aa35013e5bfa02969bb905
Stefani Gerber <stefani.gerber@bluesky-it.ch>
2017-08-14 21:42:55 +0200

    make svg (#6)

--- src/app/graphics/barchart/barchart.component.ts
+++ src/app/graphics/barchart/barchart.component.ts
@@ -4,6 +4,9 @@ import { Chart } from '../../models/chart.model';
 
 import * as d3Select from 'd3-selection';
 
+const height = 200;
+const width = 200;
+
 @Component({
   selector: 'dl-barchart',
   template: '',
@@ -16,13 +19,24 @@ export class BarchartComponent implements OnInit {
   constructor(private rootElement: ElementRef) { }
 
   ngOnInit() {
-    d3Select.select(this.rootElement.nativeElement)
-      .selectAll('div')
+    const svg = d3Select.select(this.rootElement.nativeElement)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    const itemWidth = width / this.chart.data.length;
+    const itemPadding = 1;
+    const scale = 10;
+
+    svg.selectAll('rect')
       .data(this.chart.data)
       .enter()
-      .append('div')
-      .style('height', (d) => `${d.value * 5}px`)
-      .style('background-color', (d) => d.color);
+      .append('rect')
+      .attr('x', (d, i) => i * itemWidth)
+      .attr('y', (d) => height - d.value * scale)
+      .attr('width', itemWidth - itemPadding)
+      .attr('height', (d) => d.value * scale)
+      .attr('fill', (d) => d.color || `rgb(0, 0, ${Math.round(d * 10)})`);
     }
 
 }
